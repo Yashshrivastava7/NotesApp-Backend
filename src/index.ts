c4cd6449bb17758c603d006eff245d44d5738a56
Yash Shrivastava <68471290+Yashshrivastava7@users.noreply.github.com>
2023-03-19 13:34:40 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,17 +2,16 @@ require('dotenv').config({path:'./.env'});
 import { Express, Request, Response } from 'express';
 import { UserPassSchema , NotesSchema } from './models/Models';
 import mongoose from 'mongoose';
+const jwt = require('jsonwebtoken');
+const UserPass = mongoose.model("UserPass", UserPassSchema);
+const Notes = mongoose.model("Notes", NotesSchema);
+const express = require('express');
 
 mongoose.connect("mongodb+srv://yashnode:<password>@cluster0.nbdaj.mongodb.net/?retryWrites=true&w=majority");
 
-const UserPass = mongoose.model("UserPass", UserPassSchema);
-const Notes = mongoose.model("Notes", NotesSchema);
-
-const express = require('express');
-
 const app: Express = express();
-const PORT = 8080;
-const jwt = require('jsonwebtoken');
+const port = 8080;
+
 
 app.use(express.json());
 
@@ -24,35 +23,36 @@ app.get('/user', authenticate , (req: Request , res: Response) => {
   return res.status(200).send("Success");
 });
 
-app.post('/signup', (req: Request , res: Response) => {
-  const username : string = req.body.username;
-  const password : string = req.body.password;
-  run();
-  async function run() {
-    const checker = await UserPass.find({username:username}).exec();
-    if(checker.length!==0){
-      return res.status(400).send("User already exists");
-    }
-    else {
-      await UserPass.create({username:username, password:password});
-      return res.status(200).send("User registered successfully");
-    }
+app.post("/signup", async (req: Request, res: Response) => {
+  const username: string = req.body.username;
+  const password: string = req.body.password;
+  const checker = await UserPass.find({ 
+    username: username 
+  }).exec();
+  if (checker.length !== 0) {
+    return res.status(400).send("User already exists");
+  } else {
+    await UserPass.create({ username: username, password: password });
+    return res.status(200).send("User registered successfully");
   }
 });
 
-app.post('/login', (req: Request, res: Response) => {
+app.post("/login", async (req: Request, res: Response) => {
   const username = req.body.username;
   const password = req.body.password;
-  run();
-  async function run() {
-    const checker = await UserPass.find({username:username,password:password}).exec();
-    if(checker.length===0){
-      return res.status(400).send("Invalid Credentials");
-    }
-    else {
-      const AccessToken = jwt.sign({username: username}, process.env.ACCESS_TOKEN, { expiresIn: '10m' });
-      return res.status(200).json({ AccessToken: accessToken });
-    }
+  const checker = await UserPass.find({
+    username: username,
+    password: password,
+  }).exec();
+  if (checker.length === 0) {
+    return res.status(400).send("Inlavid Credentials");
+  } else {
+    const AccessToken = jwt.sign(
+      { username: username },
+      process.env.ACCESS_TOKEN,
+      { expiresIn: "10m" }
+    );
+    return res.status(200).json({ AccessToken: AccessToken });
   }
 });
 
