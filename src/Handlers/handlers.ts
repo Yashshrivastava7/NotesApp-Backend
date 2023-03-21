cb10604224b4c7018656833c4a24f39421c73ec4
Divyansh <divyansh.shrivastava@students.iiit.ac.in>
2023-03-21 22:06:15 +0530

    Add Mongo Credentials to .env and added a new API for getting single note

--- src/Handlers/handlers.ts
+++ src/Handlers/handlers.ts
@@ -1,31 +1,35 @@
-require('dotenv').config({path:'./.env'});
-import { Express, Request, Response } from 'express';
-import { UserPassSchema , NoteSchema } from '../models/Models';
-const jwt = require('jsonwebtoken');
-import mongoose from 'mongoose';
+require("dotenv").config({ path: "./.env" });
+import { Express, Request, Response } from "express";
+import { UserPassSchema, NoteSchema } from "../models/Models";
+const jwt = require("jsonwebtoken");
+import mongoose from "mongoose";
 
-mongoose.connect("mongodb+srv://yashnodets:yashnodetsx@cluster0.jnmzzaw.mongodb.net/?retryWrites=true&w=majority");
+const mongoUsername = process.env.MONGO_USER;
+const mongoPassword = process.env.MONGO_PASSWORD;
+const mongoCluster = process.env.MONGO_CLUSTER;
+const mongoURI = `mongodb+srv://${mongoUsername}:${mongoPassword}@${mongoCluster}`;
+mongoose.connect(mongoURI);
 
 const UserPass = mongoose.model("UserPass", UserPassSchema);
 const Notes = mongoose.model("Notes", NoteSchema);
 
-exports.checkUser = (req: Request , res: Response) => {
+exports.checkUser = (req: Request, res: Response) => {
   return res.status(200).send(`Success and user is ${req.username}`);
-}
+};
 
-exports.handleSignup =  async (req: Request, res: Response) => {
+exports.handleSignup = async (req: Request, res: Response) => {
   const username: string = req.body.username;
   const password: string = req.body.password;
-  const checker = await UserPass.find({ 
-    username: username 
-    }).exec();
+  const checker = await UserPass.find({
+    username: username,
+  }).exec();
   if (checker.length !== 0) {
     return res.status(400).send("User already exists");
   } else {
     await UserPass.create({ username: username, password: password });
     return res.status(200).send("User registered successfully");
   }
-}
+};
 
 exports.handleLogin = async (req: Request, res: Response) => {
   const username = req.body.username;
@@ -44,18 +48,25 @@ exports.handleLogin = async (req: Request, res: Response) => {
     );
     return res.status(200).json({ AccessToken: AccessToken });
   }
-}
+};
 
 exports.getAllNotes = async (req: Request, res: Response) => {
   const username = req.username; //coming straight from authenticate function
-  const notes = await Notes.find({username: username}).exec();
+  const notes = await Notes.find({ username: username }).exec();
   return res.status(200).send(notes);
-}
+};
 
 exports.addNote = async (req: Request, res: Response) => {
-  await Notes.create({ username: req.username , title: req.body.title, note: req.body.note});
+  const title = req.body.title;
+  const note = req.body.note;
+  console.log(`[POST /notes] \nTitle: ${title}\nnote: ${note}`);
+  await Notes.create({
+    username: req.username,
+    title: req.body.title,
+    note: req.body.note,
+  });
   return res.status(200).send("Note added succesfully");
-}
+};
 
 exports.deleteNote = async (req: Request, res: Response) => {
   let delNote = undefined;
@@ -64,11 +75,26 @@ exports.deleteNote = async (req: Request, res: Response) => {
   } catch (e) {
     console.error("There was an error");
   }
-  
+
   console.log(delNote);
-  if(!delNote){
+  if (!delNote) {
     return res.status(400).send("Invalid Request");
   }
-  await Notes.findOneAndDelete({username: req.username, _id: req.params.id});
+  await Notes.findOneAndDelete({ username: req.username, _id: req.params.id });
   return res.status(204).send("Note deleted successfully");
-}
+};
+
+exports.getSingleNote = async (req: Request, res: Response) => {
+  const id = req.params.id;
+  console.log(`[GET /notes/${id}]`);
+  let note = undefined;
+  try {
+    note = await Notes.findById(id).exec();
+  } catch (e) {
+    console.error("Error fetching record from DB with ID: " + id);
+    return res.status(500).send("Error Fetching Record from DB");
+  }
+  console.log("Returning Note:");
+  console.log(note);
+  return res.status(200).json(note);
+};
--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,22 @@
-require('dotenv').config({path:'./.env'});
-import { Express, Request, Response } from 'express';
-const express = require('express');
-const requestHandlers = require('./Handlers/handlers');
-import authenticate from './Auth';
+require("dotenv").config({ path: "./.env" });
+import { Express, Request, Response } from "express";
+const express = require("express");
+const requestHandlers = require("./Handlers/handlers");
+import authenticate from "./Auth";
 
 const app: Express = express();
 const port = 8080;
 
-
 app.use(express.json());
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Express + TypeScript Server');
+app.get("/", (req: Request, res: Response) => {
+  res.send("Express + TypeScript Server");
 });
 
 app.get("/user", authenticate, requestHandlers.checkUser);
 app.post("/signup", requestHandlers.handleSignup);
 app.post("/login", requestHandlers.handleLogin);
+app.get("/notes/:id", authenticate, requestHandlers.getSingleNote);
 app.get("/notes", authenticate, requestHandlers.getAllNotes);
 app.post("/notes", authenticate, requestHandlers.addNote);
 app.delete("/notes/:id", authenticate, requestHandlers.deleteNote);
