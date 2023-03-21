b82968d45eb80135da39c01ab0b7bf80028670a5
Divyansh <divyansh.shrivastava@students.iiit.ac.in>
2023-03-22 01:37:22 +0530

    Cleanup Auth.ts file

--- src/Auth.ts
+++ src/Auth.ts
@@ -1,21 +1,23 @@
-require('dotenv').config({path:'./.env'});
-import { Express, Request, Response } from 'express';
-const jwt = require('jsonwebtoken');
+require("dotenv").config({ path: "./.env" });
+import { Request, Response } from "express";
+import { verify } from "jsonwebtoken";
 
-declare module 'express' {
+declare module "express" {
   export interface Request {
-    username?: string
+    username?: string;
   }
 }
 
-export default function authenticate(req : Request, res : Response, next) {
-	const authHeader = req.headers['authorization'];
-	const token : string = authHeader && authHeader.split(' ')[1];
-	if (token == null) return res.sendStatus(401)
+export default function authenticate(req: Request, res: Response, next) {
+  const authHeader = req.headers["authorization"];
+  const token: string = authHeader && authHeader.split(" ")[1];
+  if (token == null) return res.sendStatus(401);
 
-	jwt.verify(token, process.env.ACCESS_TOKEN, (err, data) => {
-		if (err) return res.sendStatus(403)
-		req.username = data.username;
-		next()
-	})
+  verify(token, process.env.ACCESS_TOKEN, (err, data: any) => {
+    if (err) {
+      return res.sendStatus(403);
+    }
+    req.username = data.username;
+    next();
+  });
 }
